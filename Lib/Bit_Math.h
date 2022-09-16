103730a23dd86bf46af6f1e0d08e5cbd8a8e9f4d
Mohamed Abdelmordy <m.mordy97@gmail.com>
2022-09-16 21:46:47 +0200

    adding library modules Bit_Math.h and Std_Types.h

---
+++ Lib/Bit_Math.h
@@ -0,0 +1,12 @@
+
+#ifndef  BIT_MATH_H
+#define  BIT_MATH_H
+
+
+#define SET_BIT(reg, bit)         ((reg) |= 1 << (bit))
+#define CLEAR_BIT(reg, bit)       ((reg) &= ~(1 << (bit)))
+#define GET_BIT(reg, bit)         ((( reg ) >> ( bit )) & 1 )
+#define TOGGLE_BIT(reg, bit)      ((reg) ^= 1 << (bit))
+
+
+#endif  //  BIT_MATH_H
